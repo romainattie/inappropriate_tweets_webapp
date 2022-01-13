3d6caff94331db025e1ce5df1f3fcffadaebcdd1
Romain ATTIE <attie.romain@gmail.com>
2022-01-13 16:34:03 +0100

    k

---
+++ keys.py
@@ -0,0 +1,7 @@
+
+keys = {
+    'access_token_key': '1478665512758489091-1o4CjxvdyPwVMeSDfGSFoTJXhPjQB7',
+    'access_token_secret': '2F6ka61TCjMrWvkgUedm36UKuz95TLX8Qg1Sy48sPywNA',
+    'consumer_key': '8I8v9ULvlB46fkpDVeKeBtDcf',
+    'consumer_secret': 'fyMoLbYkmW3U2JO8vegsuOJ4EFFPTw30H8jmGtpITfEV6kwMRO'
+}
